043f37ce625588b6cddfcedc43e30e94a3f47a2c
Fernando <fernandotrejo125@gmail.com>
2024-03-25 17:31:52 -0300

    Fixeando demo y agregandole logo

--- frontend/static/js/main.js
+++
@@ -1,9 +0,0 @@
-$("#chat-form").on("submit", function(e) {
-    e.preventDefault();
-    var userMessage = $("#user-message").val();
-    $.post("/bot", {user_message: userMessage}, function(botMessage) {
-        $("#chatbox").append("<p class='mb-2'><strong>Tú:</strong> " + userMessage + "</p>");
-        $("#chatbox").append("<p class='mb-2 text-blue-500'><strong>Bot:</strong> " + botMessage + "</p>");
-    });
-    $("#user-message").val("");
-});
